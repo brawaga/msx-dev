8eaf72839fcbe4a75db7bc6b2d6e2ea349eb799f
Kirill Bykov <brawaga@gmail.com>
2021-01-15 23:08:40 +0700

    Less utility now can get stream from stdin if no parameter given, so can be redirected or piped to.

--- less/less.c
+++ less/less.c
@@ -1,39 +1,49 @@
-
 #include <stdio.h>
+#include <msxbios.h> 
 
 int main(argc, argv)
-  int argc;
-  char **argv;
+	int argc;
+	char **argv;
 {
-  FILE *f;
-  char line[81];
-  char *result;
-  char pageSize = 20;
-  char lineCount = pageSize;
-  char *prompt = "Page: [Sp], line: [Rtn], exit: [Esc]?";
-  int i;
-  f = fopen(argv[1], "rt");
-  result = fgets(line, 80, f);
-  i = ferror(f);
-  fprintf("Error: %d\n", i);
-  while (result) {
-    if (lineCount) {
-      --lineCount;
-      puts(line);
-      result = fgets(line, 80, f);
-    } else {
-      char c;
-      fprintf(stderr, prompt);
-      c = getch();
-      switch(c) {
-        case 13: lineCount = 1; break;
-        case 32: lineCount = pageSize; break;
-        case 27: result = 0; break;
-      };
-      fprintf(stderr, "\x1BM");
-    }
-  }
-  fclose(f);
+	FILE *f;
+	char line[81];
+	char *result;
+	char pageSize = 20;
+	char lineCount = pageSize;
+	char *prompt = "Page: [Sp], line: [Rtn], exit: [Esc]?";
+	int i;
+	f = argc>1?fopen(argv[1], "rt"):stdin;
+	if (f) {
+		result = fgets(line, 80, f);
+		i = ferror(f);
+		fprintf("Error: %d\n", i);
+		while (result) {
+			if (lineCount) {
+				--lineCount;
+				puts(line);
+				result = fgets(line, 80, f);
+			} else {
+				char c;
+				fprintf(stderr, prompt);
+				c = chget();
+				switch(c) {
+				case 'K': case 'k': case 'L': case 'l':
+				case 'D': case 'd': case '1': case '!':
+				case 13:
+					lineCount = 1; break;
+				case 'N': case 'n': case 32:
+					lineCount = pageSize; break;
+				case 'Q': case 'q': case 'X': case 'x':
+				case '.': case 27:
+					result = 0; break;
+				};
+				fprintf(stderr, "\x1BM");
+			}
+		}
+		if (argc>1) fclose(f);
+	} else {
+		printf("Error: input unavailable.\n");
+	}
 }
 
 
